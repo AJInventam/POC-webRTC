3d9aa4ad402870185453b7a2b95bf6ede0438a6e
Akshay <you@example.com>
2022-04-11 13:54:44 +0530

    port change

--- src/assets/js/helpers.js
+++ src/assets/js/helpers.js
@@ -112,22 +112,15 @@ export default {
     return {
       iceServers: [
         {
-          urls: [
-            "stun:eu-turn4.xirsys.com",
-            "stun.l.google.com:19302",
-            "stun1.l.google.com:19302",
-            "stun2.l.google.com:19302",
-            "stun3.l.google.com:19302",
-            "stun4.l.google.com:19302",
-          ],
+          urls: ["stun:eu-turn4.xirsys.com"],
         },
         {
           username:
             "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl",
           credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",
           urls: [
-            "turn:eu-turn4.xirsys.com:3478?transport=tcp",
             "turn:eu-turn4.xirsys.com:80?transport=udp",
+            "turn:eu-turn4.xirsys.com:3478?transport=tcp",
           ],
         },
       ],
