0c75c3acadb9c75dcdac6192cf3ead5eeb71f9bb
Akshay <you@example.com>
2022-04-11 16:10:19 +0530

    port change

--- src/assets/js/helpers.js
+++ src/assets/js/helpers.js
@@ -112,17 +112,23 @@ export default {
     return {
       iceServers: [
         {
-          urls: ["stun:eu-turn4.xirsys.com"],
-        },
-        {
-          username:
-            "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl",
-          credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",
           urls: [
-            "turn:eu-turn4.xirsys.com:80?transport=udp",
-            "turn:eu-turn4.xirsys.com:3478?transport=tcp",
+            "stun:stun1.l.google.com:19302",
+            "stun:stun2.l.google.com:19302",
           ],
         },
+        // {
+        //   urls: ["stun:eu-turn4.xirsys.com"],
+        // },
+        // {
+        //   username:
+        //     "ml0jh0qMKZKd9P_9C0UIBY2G0nSQMCFBUXGlk6IXDJf8G2uiCymg9WwbEJTMwVeiAAAAAF2__hNSaW5vbGVl",
+        //   credential: "4dd454a6-feee-11e9-b185-6adcafebbb45",
+        //   urls: [
+        //     "turn:eu-turn4.xirsys.com:80?transport=udp",
+        //     "turn:eu-turn4.xirsys.com:3478?transport=tcp",
+        //   ],
+        // },
       ],
     };
   },
